765596d02a4fdfd4a610315314ee8a2bbcd04484
D1zom <zdimaalkino@gmail.com>
2021-06-08 13:27:24 +0300

    13:21

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+#Install ansible and play the playbook from ./TEST on L-FW
+cd ./FW
+#ansible-playbook ansible.yml
+# test PING
+#ansible-playbook ping.yml
+# begin
+#ansible-playbook begin.yml
+# gre
+#ansible-playbook gre.yml
+# ipsec
+#ansible-playbook ipsec.yml
+# dns server
+#ansible-playbook namedl.yml
+ansible-playbook namedr.yml
 
